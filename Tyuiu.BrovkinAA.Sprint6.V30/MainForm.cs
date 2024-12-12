2ae8ec51f57067cedf9ae99b385292da252e650b
BrovkinAlex <Screep_dogg@mail.ru>
2024-12-12 19:31:53 +0500

    Task6

---
+++ Tyuiu.BrovkinAA.Sprint6.V30/MainForm.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.BrovkinAA.Sprint6.V30
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
