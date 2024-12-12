49eee3edf26479285d3912b142ad306703cfca27
BrovkinAlex <Screep_dogg@mail.ru>
2024-12-12 19:27:25 +0500

    Task6

---
+++ Tyuiu.BrovkinAA.Sprint6.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.BrovkinAA.Sprint6.Task6.V30.Lib
+{
+    public class DataService : ISprint6Task6V30
+    {
+        public string CollectTextFromFile(string path)
+        {
+            string[] words;
+            string resString = "";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    resString = line.Trim().Split(' ')[3] + " ";
+                }
+            }
+
+            return resString.Trim();
+        }
+    }
+}
---
+++ Tyuiu.BrovkinAA.Sprint6.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BrovkinAA.Sprint6.Task6.V30.Lib;
+namespace Tyuiu.BrovkinAA.Sprint6.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidText()
+        {
+            DataService ds = new DataService();
+            string row = ds.CollectTextFromFile(@"D:\Downloads\InPutDataFileTask6V30.txt");
+            string wait = "PAlR qdsTS zgGQ XVZeHq dwMQ";
+        }
+    }
+}
